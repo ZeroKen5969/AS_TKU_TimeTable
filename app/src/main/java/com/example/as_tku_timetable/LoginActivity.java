c44400b246ace89c9a5b1d05ff11bfa184d471e3
ZeroKen5969 <a0940100@gmail.com>
2019-05-29 00:05:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/as_tku_timetable/LoginActivity.java
@@ -0,0 +1,83 @@
+package com.example.as_tku_timetable;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Hashtable;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView usr_editor;
+    private TextView psw_editor;
+    private Button lg_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usr_editor = findViewById(R.id.usr_editor);
+        psw_editor = findViewById(R.id.psw_editor);
+        findViewById(R.id.lg_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    //private static String LOGIN_PAGE = "https://sso.tku.edu.tw/NEAI/logineb.jsp?myurl=https://sso.tku.edu.tw/aissinfo/emis/tmw0012.aspx";
+    private String LOGIN_VIDCODE = "https://sso.tku.edu.tw/NEAI/ImageValidate";
+    private String LOGIN_ACTION = "https://sso.tku.edu.tw/NEAI/login2.do?action=EAI";
+    private String TIME_TABLE = "https://sso.tku.edu.tw/aissinfo/emis/TMWC090_result.aspx?YrSem=1072&stu_no=";
+    private WWW www = new WWW();
+    private void Login() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String response;
+                Hashtable<String, String> data;
+                //www.SendGet(LOGIN_PAGE);
+
+                /*******取得驗證碼*******/
+                www.SendGet(LOGIN_VIDCODE);
+                data = new Hashtable<String, String>();
+                data.put("outType", "2");
+                response = www.SendPost(LOGIN_VIDCODE, data);
+                //System.out.println(response);
+
+                /*******登入*******/
+                data = new Hashtable<String, String>();
+                data.put("myurl", "https://sso.tku.edu.tw/aissinfo/emis/tmw0012.aspx");
+                data.put("ln", "zh_TW");
+                data.put("embed", "No");
+                data.put("logintype", "logineb");
+                data.put("username", usr_editor.getText().toString());
+                data.put("password", psw_editor.getText().toString());
+                data.put("vidcode", response);
+                data.put("loginbtn", "登入");
+                response = www.SendPost(LOGIN_ACTION, data);
+                //System.out.println(response);
+
+                /*******進入課表畫面*******/
+                www.SendGet(TIME_TABLE + usr_editor.getText()); //要get兩次
+                response = www.SendGet(TIME_TABLE + usr_editor.getText());
+                TextView tv = findViewById(R.id.textView4);
+                tv.setText(response);
+                System.out.println(response);
+            }
+        }).start();
+        TextView tv = findViewById(R.id.textView4);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+    }
+}
---
+++ app/src/main/java/com/example/as_tku_timetable/WWW.java
@@ -0,0 +1,144 @@
+package com.example.as_tku_timetable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
+
+public class WWW {
+
+    private enum Method {
+        GET,
+        POST
+    }
+
+    private Map<String, String> headers;
+    private List<String> cookies;
+
+    public WWW() {
+        headers = new Hashtable<String, String>();
+        cookies = new LinkedList<String>();
+    }
+
+    //使用者可從這加入客製化header
+    public void AddHeader(String header, String data) {
+        if(header.equals("Cookie")) {
+            cookies.add(data);
+        } else {
+            headers.put(header, data);
+        }
+    }
+
+    //負責發送request及讀取response
+    private String ReadWebPage(HttpURLConnection con) {
+        StringBuffer buf = null;
+        try {
+            /*******設置response cookie*******/
+            Map<String, List<String>> hds = con.getHeaderFields();
+            if(hds.containsKey("Set-Cookie")) {
+                for(String cookie : hds.get("Set-Cookie")) {
+                    cookies.add(cookie.split(";", 2)[0].trim());
+                }
+            }
+
+            buf = new StringBuffer();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line = null;
+            while((line = in.readLine()) != null) {
+                buf.append(line);
+            }
+            in.close();
+
+            ///自動重定向
+            if(con.getResponseCode() == 302) {
+                String redirUrl = con.getHeaderField("location");
+                return SendGet(redirUrl);
+                //System.out.println(ul);
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return buf.toString();
+    }
+
+    //用於組裝所有cookie
+    private String CombineCookies() {
+        StringBuffer buf = new StringBuffer();
+        for (String cookie : cookies) {
+            if (buf.length() != 0) buf.append(' ');
+            buf.append(cookie);
+            if(!cookie.endsWith(";")) buf.append(";");
+        }
+        return buf.toString();
+    }
+
+    //用於製作request
+    private HttpURLConnection MakeRequest(Method method, String url, Hashtable<String,String> postData) {
+        HttpURLConnection con = null;
+        try {
+            con = (HttpURLConnection)new URL(url).openConnection();
+            con.setRequestMethod(method.name());
+
+            /*******組裝cookies*******/
+            if(cookies.size() > 0) {
+                con.setRequestProperty("Cookie", CombineCookies());
+            }
+
+            /*******設置使用者header*******/
+            for(Map.Entry<String, String> header : headers.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            /*******設置預設header*******/
+            con.setRequestProperty("User-Agent","Mozilla/5.0");
+            con.setInstanceFollowRedirects(false);
+            con.setConnectTimeout(10000);
+            con.setReadTimeout(10000);
+
+            if(method == Method.POST) {
+                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                con.setDoOutput(true);
+                StringBuffer buf = new StringBuffer();
+                /*******組裝post data*******/
+                for (Map.Entry<String, String> param : postData.entrySet()) {
+                    if (buf.length() != 0) buf.append('&');
+                    buf.append(param.getKey());
+                    buf.append('=');
+                    buf.append(param.getValue());
+                }
+                byte[] postDataBytes = buf.toString().getBytes("UTF-8");
+                con.getOutputStream().write(postDataBytes);
+            }
+            //System.out.println(con.getRequestProperties());
+            //System.out.println(con.getResponseCode());
+            //System.out.println(con.getHeaderFields());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return con;
+    }
+
+    //用來發送GET請求
+    public String SendGet(String path) {
+        HttpURLConnection con = MakeRequest(Method.GET, path, null);
+        return con != null ? ReadWebPage(con) : null;
+    }
+
+    //用來發送POST請求
+    public String SendPost(String path, Hashtable<String, String> postData) {
+        HttpURLConnection con = MakeRequest(Method.POST, path, postData);
+        return con != null ? ReadWebPage(con) : null;
+    }
+}
